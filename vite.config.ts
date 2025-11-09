bef7a2cb79bd1e85fa5defe17846bb87336f86e5
arsrna <root@arsrna.cn>
2025-11-10 01:17:00 +0800

    chore: 更新依赖版本和构建配置，升级至3.0.9版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+import path, { format } from 'path';
 import typescript from '@rollup/plugin-typescript';
 import react from "@vitejs/plugin-react";
 
@@ -52,24 +52,24 @@ export default defineConfig({
     cssCodeSplit: false,
     // 打包输出的目录
     outDir: 'dist',
-    // 防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制
-    cssTarget: 'chrome61',
     lib: {
+      formats: ['es', 'umd'],
       // 组件库源码的入口文件
       entry: resolve('packages/index.ts'),
       // 组件库名称
       name: 'index',
-      fileName: 'index',
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['react', 'react-dom', 'antd', 'typescript'],
+      external: ['react', 'react-dom'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           react: 'react',
           'react-dom': 'react-dom',
         },
+        manualChunks: undefined,
       },
     },
   }
